c805864007b0ee07bee85a53ababe4ea5ba92366
pwpdev <pramod@wbcomdesigns.com>
2018-10-06 15:02:06 +0530

    version 1.0.4 released

--- wbcom-theme-demo-installer.php
+++ wbcom-theme-demo-installer.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wbcom Theme Demo Installer
  * Plugin URI: https://wbcomdesigns.com/
  * Description: Wbcom Theme Demo Installer
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Wbcom Designs
  * Author URI: https://wbcomdesigns.com/
  * Requires at least: 4.0
@@ -34,7 +34,7 @@ class WBCOM_Theme_Demo_Installer {
 	 *
 	 * @var string
 	 */
-	public $version = '1.0.3';
+	public $version = '1.0.4';
 	
 	/**
 	 * The single instance of the class.
