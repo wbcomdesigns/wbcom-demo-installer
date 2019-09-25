771ea8a249ba4458ba2c5759a24e5feb8dcb163b
Varun Kumar Dubey <vapvarun@gmail.com>
2019-09-26 02:59:17 +0530

    Update wbcom-theme-demo-installer.php

--- wbcom-theme-demo-installer.php
+++ wbcom-theme-demo-installer.php
@@ -3,11 +3,11 @@
  * Plugin Name: Wbcom Theme Demo Installer
  * Plugin URI: https://wbcomdesigns.com/
  * Description: Wbcom Theme Demo Installer
- * Version: 1.1.0
+ * Version: 2.0.0
  * Author: Wbcom Designs
  * Author URI: https://wbcomdesigns.com/
  * Requires at least: 4.0
- * Tested up to: 5.2.2
+ * Tested up to: 5.2.3
  *
  * Text Domain: wbcom-theme-demo-installer
  * Domain Path: /i18n/languages/
@@ -38,7 +38,7 @@ class WBCOM_Theme_Demo_Installer {
 	 *
 	 * @var string
 	 */
-	public $version = '1.1.0';
+	public $version = '2.0.0';
 
 	/**
 	 * The single instance of the class.
